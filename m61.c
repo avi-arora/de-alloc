06ba49c437b0bfaef0705e51d679197f33c193ec
avishek arora <avishek.arora@icloud.com>
2017-04-27 16:02:13 +0530

    now detects boundary write off error via free. tests passed, optimised

--- m61.c
+++ m61.c
@@ -26,9 +26,13 @@ struct m61_statistics current_stats = {
 struct meta {
     size_t block_size: 31;
    char allocated: 1; // y if block is allocated in heap, else n or anything
-   char *marker;
+   char *marker;     // indicates the block is allocated by this debugging memory allocator.
+};
+struct footer {
+    char *marker;
 };
 size_t meta_header_padding = (sizeof(struct meta) % ALIGNMENT) ? (sizeof(struct meta) + ALIGNMENT) - (sizeof(struct meta) % ALIGNMENT) : sizeof(struct meta);
+size_t meta_footer_padding = (sizeof(struct footer) % ALIGNMENT) ? (sizeof(struct footer) + ALIGNMENT) - (sizeof(struct footer) % ALIGNMENT): sizeof(struct footer);
 
 
 
@@ -65,8 +69,9 @@ void update_heap_address(char *input_address, size_t sz) {
 
 void* m61_malloc(size_t sz, const char* file, int line) {
     (void) file, (void) line;   // avoid uninitialized variable warnings
-    void *starting_address = base_malloc(sz + meta_header_padding); // also allocate space for meta data.
+    void *starting_address = base_malloc(sz + meta_header_padding + meta_footer_padding); // also allocate space for meta data.
     struct meta *meta_data_ptr = starting_address; 
+    struct footer *meta_footer_ptr = (struct footer *) ((size_t)starting_address + sz + meta_header_padding);
     if(starting_address == NULL || sz + meta_header_padding < sz ) // memory allocation failed.
     {
         //updates the total number of failed memory allocation attempts.
@@ -81,13 +86,14 @@ void* m61_malloc(size_t sz, const char* file, int line) {
         meta_data_ptr->block_size = sz;
         meta_data_ptr->allocated = 1;
         meta_data_ptr->marker = (char *)MARKER;
+        meta_footer_ptr->marker = (char *)MARKER;
         current_stats.ntotal += 1; // updates every allocation, keeps track of total number of allocations.
         update_active_allocations(); //updates the current_stats, because more memory is allocated.
         current_stats.total_size += sz; // updates total bytes allocated so far. 
         update_heap_address((char*)starting_address + meta_header_padding, sz); // updates heap address space seen so far.
         current_stats.active_size += sz;
     }  
-    void *payload = starting_address + meta_header_padding;
+    void *payload = (void *) ((struct meta *) starting_address + 1);
     return payload;
 }
 
@@ -104,7 +110,7 @@ void m61_free(void *ptr, const char *file, int line) {
         return;
     void *memory = ptr - meta_header_padding;
     struct meta *meta_data_ptr = (struct meta *) memory; //computing ptr to meta data
-        
+    struct footer *meta_footer_ptr = ptr + meta_data_ptr->block_size;
      //pointer doesn't point inside heap
      if(ptr > (void *)current_stats.heap_max || ptr < (void *)current_stats.heap_min)
      {
@@ -116,20 +122,27 @@ void m61_free(void *ptr, const char *file, int line) {
      {
         printf("MEMORY BUG: %s:%d: invalid free of pointer %p, not allocated\n", file, line, ptr);
         abort();
-     }
+     } meta_data_ptr->marker = 0x0;
      //resolve's double free issues
      if(meta_data_ptr->allocated == 0)
      {
         printf("MEMORY BUG: %s:%d: invalid free of pointer %p, already free'd\n", file, line, ptr);
         abort();
      }
+     //wild write error during free of allocated memory, 
+     //stop freeing unallocated space, off by one and other write error in array's
+     //check the boundary write error of the end of the allocated block
+     if(meta_footer_ptr->marker != (char *)MARKER)
+     {
+        printf("MEMORY BUG: %s:%d: detected wild write during free of pointer %p\n", file, line, ptr);
+        abort();
+     }
      
-        
-        
      
+      
     current_stats.active_size -= meta_data_ptr->block_size; // extracting allocation size from meta data, updating active size
-    meta_data_ptr->allocated = 0;
-    base_free(ptr);  
+    meta_data_ptr->allocated = 0; //indicating that the block is now about to release, free'd.
+    base_free(memory);  
     total_free += 1; //updates the total number of free(ptr) so far. 
     update_active_allocations(); //this changes because memory is being released. 
 }
@@ -152,7 +165,15 @@ void* m61_realloc(void* ptr, size_t sz, const char* file, int line) {
         // Your code here (to fix test012).
         void *memory = ptr - meta_header_padding;
         struct meta *meta_data_ptr = (struct meta *) memory;
-        memcpy(new_ptr, ptr, meta_data_ptr->block_size);
+        size_t old_size = meta_data_ptr->block_size;
+        if(sz > old_size) // if the allocated memory is bigger, resolve boundary write error.
+        {
+            memcpy(new_ptr, ptr, old_size);
+        }
+        else // if the newly allocated memory is smaller than previously allocated, can't copy all old data into it.
+        {
+            memcpy(new_ptr, ptr, sz);
+        }
     }
     if(ptr)
         m61_free(ptr, file, line);
